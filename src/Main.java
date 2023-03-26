ca66e5f8a13d9fe22f1ee581c3ca48094ca32239
KatrinAn <anpipinakaka@gmail.com>
2023-03-26 22:11:30 +0300

    commit homework

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        First one = new First();
+        Second two = new Second();
+        one.countOccurance();
+        two.ToList();
     }
 }
